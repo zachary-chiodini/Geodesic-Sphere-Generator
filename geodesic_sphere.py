67fcbdab890b2fbf97391fad51ae68672f498cc1
zchiodin <zchiodin@cisco.com>
2022-11-17 18:29:17 -0500

    Removed set of vertices, because they are not used

--- geodesic_sphere.py
+++ geodesic_sphere.py
@@ -6,21 +6,6 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 class GeodesicSphere:
 
     golden_ratio = (1 + sqrt(5)) / 2
-    # These are the vertices of a regular icosahedron.
-    verts = {
-        ( golden_ratio / 2,  0.0             ,  0.5              ),
-        ( golden_ratio / 2,  0.0             , -0.5              ),
-        (-golden_ratio / 2,  0.0             ,  0.5              ),
-        (-golden_ratio / 2,  0.0             , -0.5              ),
-        ( 0.5             ,  golden_ratio / 2,  0.0              ),
-        (-0.5             ,  golden_ratio / 2,  0.0              ),
-        ( 0.5             , -golden_ratio / 2,  0.0              ),
-        (-0.5             , -golden_ratio / 2,  0.0              ),
-        ( 0.0             ,  0.5             ,  golden_ratio / 2 ),
-        ( 0.0             , -0.5             ,  golden_ratio / 2 ),
-        ( 0.0             ,  0.5             , -golden_ratio / 2 ),
-        ( 0.0             , -0.5             , -golden_ratio / 2 )
-    }
     # These are the faces of a regular icosahedron.
     faces = {
         ((-golden_ratio / 2,  0.0             , -0.5              ),
@@ -88,7 +73,6 @@ class GeodesicSphere:
     def __init__(self, frequency: int = 0, hollow: bool = False, thinness_factor: int = 0):
         self.w = int(frequency)
         self.t = int(thinness_factor)
-        self.p = self.verts.copy()
         self.f = self.faces.copy()
         self._tesselate()
         self._project()
@@ -96,6 +80,9 @@ class GeodesicSphere:
             self._hollow()
 
     def _hollow(self) -> None:
+        """
+        This function hollows the geodesic sphere.
+        """
         for face in self.f.copy():
             v1, v2, v3 = face
             center_x, center_y, center_z = (v1[0] + v2[0] + v3[0]) / 3, (v1[1] + v2[1] + v3[1]) / 3, (v1[2] + v2[2] + v3[2]) / 3
@@ -127,12 +114,6 @@ class GeodesicSphere:
                 projected_face.append((x / m, y / m, z / m))
             self.f.remove(face)
             self.f.add(tuple(projected_face))
-        for vert in self.p.copy():
-            x, y, z = vert
-            m = sqrt(x**2 + y**2 + z**2)
-            projected_vert = (x / m, y / m, z / m)
-            self.p.remove(vert)
-            self.p.add(projected_vert)
         return None
 
     def _tesselate(self) -> None:
@@ -145,7 +126,6 @@ class GeodesicSphere:
                 mid_point1 = ((v1[0] + v2[0]) / 2, (v1[1] + v2[1]) / 2, (v1[2] + v2[2]) / 2)
                 mid_point2 = ((v2[0] + v3[0]) / 2, (v2[1] + v3[1]) / 2, (v2[2] + v3[2]) / 2)
                 mid_point3 = ((v3[0] + v1[0]) / 2, (v3[1] + v1[1]) / 2, (v3[2] + v1[2]) / 2)
-                self.p.update([mid_point1, mid_point2, mid_point3])
                 face1 = (v1, mid_point1, mid_point3)
                 face2 = (mid_point1, v2, mid_point2)
                 face3 = (mid_point2, v3, mid_point3)
@@ -179,10 +159,10 @@ class GeodesicSphere:
 
     def plot(self) -> None:
         ax = axes(projection='3d')
-        ax.scatter(*zip(*self.p), alpha=0)
         ax.add_collection3d(Poly3DCollection(verts=list(self.f), facecolors='white', edgecolor='black'))
         ax.axis('off')
         ax.grid(visible=None)
+        ax.set(xlim=(-1, 1), ylim=(-1, 1), zlim=(-1, 1))
         ax.set_aspect('equal', 'box')
         show()
         return None
